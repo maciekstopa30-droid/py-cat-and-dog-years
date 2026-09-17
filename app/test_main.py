03150260549598a46679ca2f0094933bf4949c4b
Maciej Stopa <m.stopa30@onet.pl>
2026-09-17 14:51:58 +0200

    Add boundary tests for cat and dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,20 @@
 import pytest
 
-from app.main import get_human_age
+from app import main
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
         (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
+        (14, 15, [0, 1]),
+        (15, 14, [1, 0]),
+        (23, 24, [1, 2]),
+        (24, 23, [2, 1]),
+        (27, 28, [2, 2]),
         (28, 28, [3, 2]),
-        (29, 29, [3, 3]),
+        (28, 29, [3, 3]),
+        (29, 28, [3, 2]),
         (100, 100, [21, 17]),
     ],
 )
@@ -22,4 +23,4 @@ def test_get_human_age(
     dog_age: int,
     expected: list[int],
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+    assert main.get_human_age(cat_age, dog_age) == expected
